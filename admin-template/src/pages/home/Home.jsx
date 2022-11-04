d88a1ee62ad561ec8cf0ff719c747d8fd36b1291
tonyclem <tonyclem001@gmail.com>
2022-11-04 16:32:10 +0100

    added transaction table list

--- admin-template/src/pages/home/Home.jsx
+++ admin-template/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 const Home = () => {
   return (
@@ -24,7 +25,7 @@ const Home = () => {
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
-          {/* <Table /> */}
+          <Table />
         </div>
       </div>
     </div>
