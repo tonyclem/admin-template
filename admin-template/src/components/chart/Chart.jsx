a94ed4f47841ce89fc5cc4f4bf2f91f175bb4860
tonyclem <tonyclem001@gmail.com>
2022-11-04 16:16:11 +0100

    added total revenue and monthly charts

---
+++ admin-template/src/components/chart/Chart.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+// data
+const data = [
+  {
+    name: "January",
+    total: 3200,
+  },
+  {
+    name: "February",
+    Total: 2100,
+  },
+  {
+    name: "March",
+    Total: 2000,
+  },
+  {
+    name: "April",
+    Total: 2780,
+  },
+  {
+    name: "May",
+    Total: 1890,
+  },
+  {
+    name: "June",
+    Total: 3000,
+  },
+];
+
+const Chart = () => {
+  return (
+    <div className="chart">
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <Tooltip />
+
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
--- admin-template/src/pages/home/Home.jsx
+++ admin-template/src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -15,6 +17,14 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          {/* <Table /> */}
         </div>
       </div>
     </div>
