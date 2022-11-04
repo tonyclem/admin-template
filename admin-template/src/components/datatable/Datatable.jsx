73ed1758cb3d6a3bffbd4a3eadb2e88097321dea
tonyclem <tonyclem001@gmail.com>
2022-11-04 22:00:57 +0100

    added light and dark mode, and deleted function

--- admin-template/src/components/datatable/Datatable.jsx
+++ admin-template/src/components/datatable/Datatable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./datatable.scss";
 
 import { DataGrid } from "@mui/x-data-grid";
@@ -7,18 +7,29 @@ import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
 
 const Datatable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
   const activeColumn = [
     {
       field: "active",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
           </div>
         );
       },
@@ -38,7 +49,8 @@ const Datatable = () => {
         </Link>
       </div>
       <DataGrid
-        rows={userRows}
+        className="datagrid"
+        rows={data}
         columns={userColumns.concat(activeColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
--- admin-template/src/components/navbar/Navbar.jsx
+++ admin-template/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
@@ -7,8 +7,10 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -24,7 +26,7 @@ const Navbar = () => {
           <div className="item">
             <DarkModeOutlinedIcon
               className="icon"
-              // onClick={() => dispatch({ type: "TOGGLE" })}
+              onClick={() => dispatch({ type: "TOGGLE" })}
             />
           </div>
           <div className="item">
