3dc90e8c887ee0991218d5d554bad7ae04638a99
tonyclem <tonyclem001@gmail.com>
2022-11-04 20:59:02 +0100

    added functionality that handle new user and that upload products

---
+++ admin-template/src/formSource.js
@@ -0,0 +1,70 @@
+export const userInputs = [
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+1 234 567 89",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "USA",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "USA",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
